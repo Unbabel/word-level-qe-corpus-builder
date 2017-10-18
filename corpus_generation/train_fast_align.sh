7d5a0fc2df50988a6a513081b03de3373b14a832
Ramón Astudillo <ramon@astudillo.com>
2017-10-18 18:51:54 +0100

    Completes fast_align trains script
    All data stored in word_folder
    
    The script can be called from any folder level

--- corpus_generation/train_fast_align.sh
+++ corpus_generation/train_fast_align.sh
@@ -3,9 +3,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# See the README for instructions on how to install this
-path_fast_align=./external_tools/fast_align-master/build/
+# Get root of the tools
+SCRIPT_FOLDER="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+ROOT_FOLDER=$(realpath "$SCRIPT_FOLDER/../")
 
+# See the README for instructions on how to install this
+path_fast_align="${ROOT_FOLDER}/corpus_generation/external_tools/fast_align-master/build/"
+
+# ARGUMENT HANDLING
 source_sentences=$1
 target_sentences=$2
 work_folder=$3
@@ -15,23 +20,30 @@ dataset=$(basename $target_sentences)
 
 # Create work folder
 if [ ! -d "${work_folder}" ];then
+    echo "mkdir -p ${work_folder}"
     mkdir -p ${work_folder}
 fi
+
+# PRE-PROCESS 
 
 # Generate source ||| target pairs
 paste -d '\t' $source_sentences $target_sentences > ${work_folder}/${dataset}.src-pe
 sed -i 's/\t/ ||| /g' ${work_folder}/${dataset}.src-pe
 
-# Train fast align
-# Forward
+# TRAIN FAST ALIGN
+
+# Source to Target 
 ${path_fast_align}/fast_align \
 	-i ${work_folder}/${dataset}.src-pe \
 	-d -o -v \
-    -p ./external_tools/config/a.s2t.params \
+    -p ${work_folder}/a.s2t.params \
     > ${work_folder}/${dataset}.s2t.src-pe.align
-# Backward
+
+# Target to Source 
 ${path_fast_align}/fast_align \
 	-i ${work_folder}/${dataset}.src-pe \
-	-d -o -v \
-    -p ./external_tools/config/a.t2s.params \
+	-d -o -v -r \
+    -p ${work_folder}/a.t2s.params \
     > ${work_folder}/${dataset}.t2s.src-pe.align
+
+echo "Trained model stored under ${work_folder}/"
