cb1de5c34f0d66558e92b5d36c2f6b1bc86a8ee6
Ramón Astudillo <ramon@astudillo.com>
2017-10-31 14:40:41 +0000

    Generate Gap / Source BAD tags

--- corpus_generation/generate_BAD_tags.py
+++ corpus_generation/generate_BAD_tags.py
@@ -1,9 +1,13 @@
 # TODO: Proper instaler for the tools
+import argparse
 from collections import defaultdict
 import sys
 sys.path.append('..')
 from tools.io import read_file, purple
 
+
+GAP_ERRORS = True
+SOURCE_ERRORS = True
 
 def check_out_of_bounds(tokens, alignments, source=True):
     """
@@ -20,25 +24,76 @@ def check_out_of_bounds(tokens, alignments, source=True):
         else:
             max_index = max([x[1] for x in alignments[sent_index]])
         if max_index >= length:
-            import ipdb;ipdb.set_trace(context=50)
             raise Exception(
                 "Tercom alignments and original tokens do not match."
                 "Likely an enconding problem"
             )
 
 
-if __name__ == '__main__':
+def parse_arguments(sys_argv):
 
-    # Data
-    src_file, mt_file, pe_file, src_pe_align_file, mt_pe_align_file = \
-        sys.argv[1:]
+    parser = argparse.ArgumentParser(
+        prog='New word-level tags version 0.0.1'
+    )
+    # Arguments defining a time slice
+    parser.add_argument(
+        '--in-source-segments',
+        help='One sentence per line',
+        required=True,
+        type=str
+    )
+    parser.add_argument(
+        '--in-mt-segments',
+        help='One sentence per line',
+        required=True,
+        type=str
+    )
+    parser.add_argument(
+        '--in-pe-segments',
+        help='One sentence per line',
+        required=True,
+        type=str
+    )
+    parser.add_argument(
+        '--in-source-pe-alignments',
+        help='Fast align format',
+        required=True,
+        type=str
+    )
+    parser.add_argument(
+        '--in-pe-mt-alignments',
+        help='Fast align format, deletions insertions as empty index',
+        required=True,
+        type=str
+    )
 
-    # Read data
-    source_tokens = read_file(src_file)
-    mt_tokens = read_file(mt_file)
-    pe_tokens = read_file(pe_file)
-    src_pe_alignments = read_file(src_pe_align_file, alignments=True)
-    pe_mt_alignments = read_file(mt_pe_align_file, alignments=True)
+    # OUTPUTS
+    parser.add_argument(
+        '--out-source-tags',
+        help='Source OK/BAD tags per sentence',
+        type=str
+    )
+    parser.add_argument(
+        '--out-target-tags',
+        help='Target OK/BAD tags per sentence',
+        required=True,
+        type=str
+    )
+    args = parser.parse_args(sys_argv)
+
+    if SOURCE_ERRORS:
+        assert args.out_source_tags, "--out-source-tags required"
+
+    return args
+
+
+def read_data(args):
+
+    source_tokens = read_file(args.in_source_segments)
+    mt_tokens = read_file(args.in_mt_segments)
+    pe_tokens = read_file(args.in_pe_segments)
+    src_pe_alignments = read_file(args.in_source_pe_alignments, alignments=True)
+    pe_mt_alignments = read_file(args.in_pe_mt_alignments, alignments=True)
 
     # Sanity Checks
     # Number of sentences matches
@@ -57,32 +112,47 @@ if __name__ == '__main__':
     check_out_of_bounds(pe_tokens, pe_mt_alignments, source=True)
 
     # Reorganize source-target alignments as a dict
-    target2source = []
+    pe2source = []
     for sent in src_pe_alignments:
-        target2source_sent = defaultdict(list)
+        pe2source_sent = defaultdict(list)
         for src_idx, pe_idx in sent:
-            target2source_sent[pe_idx].append(src_idx)
-        target2source.append(target2source)
+            pe2source_sent[pe_idx].append(src_idx)
+        pe2source.append(pe2source_sent)
+
+    return (
+        source_tokens, mt_tokens, pe_tokens, pe2source, pe_mt_alignments
+    )
+
+
+def get_quality_tags(mt_tokens, pe_tokens, pe_mt_alignments, pe2source):
 
     # Word + Gap Tags
-    target_sentence_tags = []
-    for sentence_index in range(num_sentences):
-        word_tags = []
-        word_deletion_indices = []
+    target_tags = []
+    source_tags = []
+    for sentence_index in range(len(mt_tokens)):
+
+        # Variables for this sentence
+        sent_tags = []
+        sent_deletion_indices = []
+        source_sentence_bad_indices = set()
         mt_position = 0
+
         for pe_idx, mt_idx in pe_mt_alignments[sentence_index]:
 
-            # Insertion / Substitution errors
             if mt_idx is None:
 
-                # Deleted word (need to store for later)
-                word_deletion_indices.append(mt_position-1)
-                pass
+                # Deleted word error (need to store for later)
+                sent_deletion_indices.append(mt_position-1)
+
+                if SOURCE_ERRORS:
+                    # Aligned words in the source are BAD
+                    source_sentence_bad_indices |= \
+                        set(pe2source[sentence_index][pe_idx])
 
             elif pe_idx is None:
 
                 # Insertion error
-                word_tags.append('BAD')
+                sent_tags.append('BAD')
                 mt_position += 1
 
             elif (
@@ -91,21 +161,82 @@ if __name__ == '__main__':
             ):
 
                 # Substitution error
-                word_tags.append('BAD')
+                sent_tags.append('BAD')
                 mt_position += 1
+
+                if SOURCE_ERRORS:
+                    # Aligned words in the source are BAD
+                    source_sentence_bad_indices |= \
+                        set(pe2source[sentence_index][pe_idx])
 
             else:
 
                 # OK
-                word_tags.append('OK')
+                sent_tags.append('OK')
                 mt_position += 1
 
         # Insert deletion errors as gaps
-        word_and_gaps_tags = []
-        for index in enumerate(word_tags):
-            if index in word_deletion_indices:
-                word_and_gaps_tags.extend([word_tags[index], 'BAD'])
-            else:
-                word_and_gaps_tags.extend([word_tags[index], 'OK'])
+        if GAP_ERRORS:
+            word_and_gaps_tags = []
+            for index, tag in enumerate(sent_tags):
+                if index in sent_deletion_indices:
+                    word_and_gaps_tags.extend([tag, 'BAD'])
+                else:
+                    word_and_gaps_tags.extend([tag, 'OK'])
+            target_tags.append(word_and_gaps_tags)
+        else:
+            target_tags.append(sent_tags)
 
-        target_sentence_tags.append(word_and_gaps_tags)
+        if SOURCE_ERRORS:
+            # Convert BAD source indices into indices
+            source_sentence_bad_tags = \
+                ['OK'] * len(source_tokens[sentence_index])
+            for index in list(source_sentence_bad_indices):
+                source_sentence_bad_tags[index] = 'BAD'
+            source_tags.append(source_sentence_bad_tags)
+
+    # Basic sanity checks
+    assert all(
+        [len(aa)*2 == len(bb) for aa, bb in zip(mt_tokens, target_tags)]
+    ), "tag creation failed"
+    assert all(
+        [len(aa) == len(bb) for aa, bb in zip(source_tokens, source_tags)]
+    ), "tag creation failed"
+
+    return source_tags, target_tags
+
+
+def write_tags(output_file, tags):
+    with open(output_file, 'w') as fid:
+        for sent_tags in tags:
+            tags_line = " ".join(sent_tags)
+            fid.write("%s\n" % tags_line)
+
+
+if __name__ == '__main__':
+
+    # ARGUMENT HANDLING
+    args = parse_arguments(sys.argv[1:])
+
+    # READ DATA AND CHECK INTEGRITY
+    (
+        source_tokens,
+        mt_tokens,
+        pe_tokens,
+        pe2source,
+        pe_mt_alignments
+    ) = read_data(args)
+
+
+    # GET TAGS FOR SOURCE AND TARGET
+    source_tags, target_tags = get_quality_tags(
+        mt_tokens,
+        pe_tokens,
+        pe_mt_alignments,
+        pe2source
+    )
+
+    # WRITE DATA
+    if SOURCE_ERRORS:
+        write_tags(args.out_source_tags, source_tags)
+    write_tags(args.out_target_tags, target_tags)
--- corpus_generation/wmt2017_tags.sh
+++ corpus_generation/wmt2017_tags.sh
@@ -16,33 +16,38 @@ in_fast_align_folder="../DATA/fast_align_models/en_de/"
 # Outputs 
 out_temporal_folder="../DATA/temporal_files/"
 out_mt_alignments="$out_temporal_folder/train.src-pe.alignments"
-out_edit_alignments="$out_temporal_folder/train.mt-pe.edit_alignments"
+out_edit_alignments="$out_temporal_folder/train.pe-mt.edit_alignments"
+# Final tags
+out_source_tags="$out_temporal_folder/source.tags"
+out_target_tags="$out_temporal_folder/target.tags"
 
 # For fast align models
-#[ -d "$out_temporal_folder" ] && rm -R "$out_temporal_folder"
-#mkdir "$out_temporal_folder"    
+[ -d "$out_temporal_folder" ] && rm -R "$out_temporal_folder"
+mkdir "$out_temporal_folder"    
 
 # Generate source-target alignments
 echo "Generating alignments"
-#bash ./align.sh \
-#    $in_source_file \
-#    $in_pe_file \
-#    $in_fast_align_folder \
-#    $out_temporal_folder/fast_align/ \
-#    $out_mt_alignments
+bash ./align.sh \
+    $in_source_file \
+    $in_pe_file \
+    $in_fast_align_folder \
+    $out_temporal_folder/fast_align/ \
+    $out_mt_alignments
 
 # Generate tercom target-side alignments 
 echo "Generating Tercom alignments"
-#bash ./tercom.sh \
-#    $in_mt_file \
-#    $in_pe_file \
-#    $out_temporal_folder/tercom/ \
-#    $out_edit_alignments
+bash ./tercom.sh \
+    $in_mt_file \
+    $in_pe_file \
+    $out_temporal_folder/tercom/ \
+    $out_edit_alignments
 
 # Generate OK/BAD tags
 python ./generate_BAD_tags.py \
-    $in_source_file \
-    $in_mt_file \
-    $in_pe_file \
-    $out_temporal_folder/train.src-pe.alignments \
-    $out_temporal_folder/train.mt-pe.edit_alignments
+    --in-source-segments $in_source_file \
+    --in-mt-segments $in_mt_file \
+    --in-pe-segments $in_pe_file \
+    --in-source-pe-alignments $out_mt_alignments \
+    --in-pe-mt-alignments $out_edit_alignments \
+    --out-source-tags $out_source_tags \
+    --out-target-tags $out_target_tags
