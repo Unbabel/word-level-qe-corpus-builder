6ef14d314ac8a146606ab584a6c20e9f5c6d4032
Erick Fonseca <erickrfonseca@gmail.com>
2020-03-13 21:56:39 +0000

    generating file names as used in the shared task

--- corpus_generation/tools/get_tags.sh
+++ corpus_generation/tools/get_tags.sh
@@ -15,10 +15,11 @@ in_fast_align_folder=$4
 out_temporal_folder=$5
 out_folder=$6
 fluency_rule=$7
+prefix=$8
 
-out_src_pe_alignments=${out_folder}/$(basename $in_source_file)-pe
-out_src_mt_alignments=${out_folder}/$(basename $in_source_file)-mt
-out_edit_alignments=${out_folder}/$(basename $in_pe_file)-mt
+out_src_pe_alignments=${out_folder}/${prefix}.src-pe
+out_src_mt_alignments=${out_folder}/${prefix}.src-mt.alignments
+out_edit_alignments=${out_folder}/${prefix}.pe-mt
 out_source_tags=${out_folder}/source_tags
 out_target_tags=${out_folder}/tags
 
@@ -75,4 +76,17 @@ bash ./tools/tercom.sh \
     true
 
 tail -n +3 ${out_temporal_folder}/tercom/out_tercom_file.ter \
-    | awk '{if ($4 > 1) hter=1; else hter=$4; printf "%.6f\n",hter}' > ${out_folder}/tercom/hter
+    | awk '{if ($4 > 1) hter=1; else hter=$4; printf "%.6f\n",hter}' > ${out_folder}/hter
+
+if [ ! -z "$prefix" ]
+then
+  cp ${in_source_file} ${out_folder}/${prefix}.src
+  cp ${in_mt_file} ${out_folder}/${prefix}.mt
+  cp ${in_pe_file} ${out_folder}/${prefix}.pe
+  mv ${out_folder}/hter ${out_folder}/${prefix}.hter
+  mv ${out_folder}/tags ${out_folder}/${prefix}.tags
+  mv ${out_folder}/source_tags ${out_folder}/${prefix}.source_tags
+  rm ${out_folder}/source_tags.json
+  rm ${out_folder}/${prefix}.pe-mt
+  rm ${out_folder}/${prefix}.src-pe
+fi
